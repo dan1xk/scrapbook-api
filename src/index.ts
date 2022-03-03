548889b4213b54edb794ac29176621d9d20f4db3
dann <sdonatordaniel@gmail.com>
2022-03-03 01:47:33 -0300

    cadastro

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
-import Usuarios from './classes/Usuario'
+import Usuario from './classes/Usuario'
 
 const app = express();
 const port = process.env.PORT || 8080;
-const usuarios: Usuarios[] = [];
+const usuarios: Usuario[] = [];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -13,10 +13,44 @@ app.use(cors());
 app.get('/', (request: Request, response: Response) => {
     return response.send('OK');
 });
-
+//mostrar usuarios
 app.get('/cadastrar', (request: Request, response: Response, next: NextFunction) => {
     return response.json(usuarios)
 });
+
+//cadastrar usuario
+app.post('/cadastrar', verificacaoDeCadastro, (request: Request, response: Response, next: NextFunction) => {
+    return response.status(201).json({
+        mensagem: 'Conta criada com sucesso.'
+    });
+});
+
+function verificacaoDeCadastro(request: Request, response: Response, next: NextFunction) {
+    const { nome , senha } = request.body;
+    const usuario = new Usuario(nome, senha);
+    let validate = true;
+
+    if(!nome || !senha) {
+        return validate = false,
+        response.json({
+            mensagem: 'Preencha todos os campos.'
+        });
+    };
+
+    if(usuario.senha.length <= 3) {
+        return validate = false,
+        response.json({
+            mensagem: 'Sua senha precisa ter mais de 3 caracteres.'
+        });
+    };
+
+    if(validate == true) {
+        return usuarios.push(usuario),
+        next();
+    }
+
+}
+
 
 app.listen(port, () => {
     console.log('Api ligada');
