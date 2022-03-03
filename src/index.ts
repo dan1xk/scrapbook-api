03db709625b81b4608f6d54c29fae2eb5cdf152e
dann <sdonatordaniel@gmail.com>
2022-03-03 01:16:18 -0300

    cadastrar

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import Usuarios from './classes/Usuario'
 
 const app = express();
 const port = process.env.PORT || 8080;
+const usuarios: Usuarios[] = [];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -10,6 +12,10 @@ app.use(cors());
 
 app.get('/', (request: Request, response: Response) => {
     return response.send('OK');
+});
+
+app.get('/cadastrar', (request: Request, response: Response, next: NextFunction) => {
+    return response.json(usuarios)
 });
 
 app.listen(port, () => {
