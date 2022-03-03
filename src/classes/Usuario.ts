5f97069e633e0ca3afa8f7a97a966f994cb61f37
dann <sdonatordaniel@gmail.com>
2022-03-03 01:12:04 -0300

    add classes

--- src/classes/Usuario.ts
+++ src/classes/Usuario.ts
@@ -1,9 +1,9 @@
 import Recado from './Recado';
 
-class Usuario {
+export default class Usuario {
     id: number;
     recado: Recado[] = []
-    
+
     constructor(public nome: string, public senha: string) {
         this.id = Math.floor(Math.random() * 1000);
     }
