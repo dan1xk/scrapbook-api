fad4c2fa0156181a44569708b9bc7c6391ef837c
dann <sdonatordaniel@gmail.com>
2022-03-03 02:10:47 -0300

    add login

--- src/index.ts
+++ src/index.ts
@@ -15,18 +15,18 @@ app.get('/', (request: Request, response: Response) => {
 });
 //mostrar usuarios
 app.get('/cadastrar', (request: Request, response: Response, next: NextFunction) => {
-    return response.json(usuarios)
+    return response.json(usuarios);
 });
 
 //cadastrar usuario
-app.post('/cadastrar/:nome/:senha', verificacaoDeCadastro, (request: Request, response: Response, next: NextFunction) => {
+app.post('/cadastrar', verificacaoDeCadastro, (request: Request, response: Response) => {
     return response.status(201).json({
         mensagem: 'Conta criada com sucesso.'
     });
 });
 
 function verificacaoDeCadastro(request: Request, response: Response, next: NextFunction) {
-    const { nome , senha } = request.params;
+    const { nome , senha } = request.body;
     const usuario = new Usuario(nome, senha);
     let validate = true;
 
@@ -47,10 +47,33 @@ function verificacaoDeCadastro(request: Request, response: Response, next: NextF
     if(validate == true) {
         return usuarios.push(usuario),
         next();
-    }
+    };
+};
 
+//logar
+app.get('/login', autenticarLogin, (request: Request, response: Response) => {
+    return response.json({
+        mensagem: 'Logado com sucesso.'
+    });
+});
+
+function autenticarLogin(request: Request, response: Response, next: NextFunction) {
+    const { nome , senha } = request.body;
+    let validate = true;
+    const usuarioNome = usuarios.find(usuario => usuario.nome === nome);
+    const usuarioSenha = usuarios.find(usuario => usuario.senha === senha);
+
+    if (!usuarioNome || !usuarioSenha) {
+        return validate = false,
+        response.json({
+            mensagem: 'Usuario ou senha incorreto.'
+        });
+    };
+
+    if(validate == true) {
+        return next();
+    };
 }
-
 
 app.listen(port, () => {
     console.log('Api ligada');
