4196d47dc5eb41ac8dc67b648ef669a51b468c45
dann <sdonatordaniel@gmail.com>
2022-03-03 01:04:45 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get('/', (request: Request, response: Response) => {
     return response.send('OK');
